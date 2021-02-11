24d50bba80231b8c2fde96c32152b1a9e1acd17d
Riley Fournier <rileyjfournier@gmail.com>
2021-02-11 04:29:21 +0000

    sum recursion complete!

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+if (fromN === toN) {
+  return toN;
+} else {
+  return fromN + sum(fromN + 1, toN)
 }
+};
+
+console.log(sum(3, 7));
 
 module.exports = sum;
+
+/*
+
+(3,7)
+is 3 = 7? no,
+3 + => {22}
+(4,7)
+is 4 = 7? no,
+4 + => {18}
+(5,7)
+is 5 = 7? no,
+5 + => {13}
+(6,7)
+is 6 = 7? no,
+6 + => {7}  <---
+(7,7)           |
+is 7 = 7? yes   |
+7 --------------
+
+*/
